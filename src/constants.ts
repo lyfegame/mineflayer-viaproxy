59eced28077ee2bbe39fb5e06203a62f80c722a8
GenerelSchwerz <rla32850@uga.edu>
2024-10-10 13:12:58 -0400

    update to JAVA_CMD

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_VIAPROXY_URL = "https://ci.viaversion.com/view/Platforms/job/ViaProxy/lastStableBuild" //"https://github.com/ViaVersion/ViaProxy";
 export const BASE_GEYSER_URL = "https://download.geysermc.org/v2/projects/geyser";
-export const VIA_PROXY_CMD = (java_loc: string, loc: string, cli=true) => java_loc + " -jar " + loc + (cli ? " cli" : "");
+export const VIA_PROXY_CMD = (java_loc: string, loc: string, cli=true) => java_loc + " -jar " + `"${loc}"` + (cli ? " cli" : "");
